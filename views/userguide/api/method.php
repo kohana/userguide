64091ae3844c62c3cccd37ac41ba71afb5991c82
Woody Gilk <woody.gilk@kohanaphp.com>
2009-09-14 13:36:48 -0500

    Only show return values if they are defined

--- views/userguide/api/method.php
+++ views/userguide/api/method.php
@@ -5,12 +5,14 @@
 <?php echo $doc->description ?>
 </div>
 
+<?php if ($doc->return): ?>
 <h6>Returns:</h6>
 <ul class="return">
 <?php foreach ($doc->return as $set): list($type, $text) = $set; ?>
 <li><code><?php echo $type ?></code> <?php echo $text ?></li>
 <?php endforeach ?>
 </ul>
+<?php endif ?>
 
 <h6>Source:</h6>
 <pre><code><?php echo Kodoc::source($doc->class->getFilename(), $doc->method->getStartLine(), $doc->method->getEndLine()) ?></code></pre>
