f57588b83964d265021e13debb24b8c5591836e2
Woody Gilk <woody.gilk@kohanaphp.com>
2009-09-13 22:32:19 -0500

    Added properties and source to API method view

--- views/userguide/api/method.php
+++ views/userguide/api/method.php
@@ -1,5 +1,5 @@
 <div class="method">
-<h3 id="<?php echo $doc->method->name ?>"><?php echo $doc->class->name, '::', $doc->method->name ?></h3>
+<h3 id="<?php echo $doc->method->name ?>"><?php echo $doc->method->name ?></h3>
 
 <div class="description">
 <?php echo $doc->description ?>
@@ -11,6 +11,9 @@
 <li><code><?php echo $type ?></code> <?php echo $text ?></li>
 <?php endforeach ?>
 </ul>
+
+<h6>Source:</h6>
+<pre><code><?php echo Kodoc::source($doc->class->getFilename(), $doc->method->getStartLine(), $doc->method->getEndLine()) ?></code></pre>
 
 <?php if ($doc->tags) echo View::factory('userguide/api/tags')->set('tags', $doc->tags) ?>
 </div>
