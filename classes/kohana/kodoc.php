ea03b52d6df35199f553366c8be44966f448cbfc
Woody Gilk <woody.gilk@kohanaphp.com>
2009-09-13 22:31:52 -0500

    Added detabbing to Kodoc::source()

--- classes/kohana/kodoc.php
+++ classes/kohana/kodoc.php
@@ -91,7 +91,19 @@ class Kohana_Kodoc {
 	{
 		$file = file($file, FILE_IGNORE_NEW_LINES);
 
-		return implode("\n", array_slice($file, $start - 1, $end - 1));
+		$file = array_slice($file, $start - 1, $end - $start + 1);
+
+		if (preg_match('/^(\s+)/', $file[0], $matches))
+		{
+			$padding = strlen($matches[1]);
+
+			foreach ($file as & $line)
+			{
+				$line = substr($line, $padding);
+			}
+		}
+
+		return implode("\n", $file);
 	}
 
 	public $class;
