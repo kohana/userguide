ce71780cf972cec8f2bae8a754015aa4b92b31f1
Woody Gilk <woody.gilk@kohanaphp.com>
2009-10-28 07:56:08 +0800

    Proper escaping of code blocks, thanks s1kx

--- views/userguide/api/method.php
+++ views/userguide/api/method.php
@@ -9,14 +9,14 @@
 <h6>Returns:</h6>
 <ul class="return">
 <?php foreach ($doc->return as $set): list($type, $text) = $set; ?>
-<li><code><?php echo $type ?></code> <?php echo $text ?></li>
+<li><code><?php echo HTML::chars($type) ?></code> <?php echo HTML::chars($text) ?></li>
 <?php endforeach ?>
 </ul>
 <?php endif ?>
 
 <?php if ($doc->source): ?>
 <h6>Source:</h6>
-<pre><code><?php echo $doc->source ?></code></pre>
+<pre><code><?php echo HTML::chars($doc->source) ?></code></pre>
 <?php endif ?>
 
 <?php if ($doc->tags) echo View::factory('userguide/api/tags')->set('tags', $doc->tags) ?>
