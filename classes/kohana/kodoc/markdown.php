kohana/userguide

commit 94c8573c1732f05404e75fef6253e25a0fef507c

Do not add the base URL to links that are only #id links

classes/kohana/kodoc/markdown.php
@@ -79,7 +79,7 @@ class Kohana_Kodoc_Markdown extends MarkdownExtra_Parser {
 	 */
 	public function doBaseURL($text)
 	{
-		return preg_replace_callback('#(?!!)\[(.+?)\]\((\S*(?:\s*".+?")?)\)#', array($this, '_add_base_url'), $text);
+		return preg_replace_callback('~(?!!)\[(.+?)\]\(((?<=#)\S*(?:\s*".+?")?)\)~', array($this, '_add_base_url'), $text);
 	}
 
 	public function _add_base_url($matches)

commit c02ac1fc423011b30dfbad3ff56ea92ddddf1bcb

Fixing a problem with attaching the base URL to links introduced in 94c8573c1732f05404e75fef6253e25a0fef507c

classes/kohana/kodoc/markdown.php
@@ -79,7 +79,7 @@ class Kohana_Kodoc_Markdown extends MarkdownExtra_Parser {
 	 */
 	public function doBaseURL($text)
 	{
-		return preg_replace_callback('~(?!!)\[(.+?)\]\(((?<=#)\S*(?:\s*".+?")?)\)~', array($this, '_add_base_url'), $text);
+		return preg_replace_callback('~(?!!)\[(.+?)\]\(([^#]\S*(?:\s*".+?")?)\)~', array($this, '_add_base_url'), $text);
 	}
 
 	public function _add_base_url($matches)