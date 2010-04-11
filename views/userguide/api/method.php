63623f91223fe4854ff5c23ad8867602f8bc618d
Woody Gilk <woody.gilk@kohanaphp.com>
2010-04-11 18:42:29 +0200

    Cleaned up class and methods views of the API browser, slightly more organized

--- views/userguide/api/method.php
+++ views/userguide/api/method.php
@@ -1,21 +1,19 @@
 <div class="method">
-	
+
+<?php $declares = $doc->method->getDeclaringClass(); ?>
 <h3 id="<?php echo $doc->method->name ?>">
-	<?php
-	echo $doc->modifiers, $doc->method->name,
-	' ( ',
-	( $doc->params ) ? $doc->params_short() : '' ,
-	' ) ', 
-	'<br/><small>declared by '.html::anchor($route->uri(array('class'=>$doc->method->getDeclaringClass()->getName())),$doc->method->getDeclaringClass()->getName()).'</small>' ?>
+	<?php echo $doc->modifiers, $doc->method->name ?>( <?php echo $doc->params ? $doc->params_short() : '' ?>)
+	<br/><small>&rsaquo; <?php echo html::anchor($route->uri(array('class' => $declares->name)), $declares->name) ?></small>
 </h3>
 
 <div class="description">
 <?php echo $doc->description ?>
 </div>
 
+<?php if ($doc->tags) echo View::factory('userguide/api/tags')->set('tags', $doc->tags) ?>
+
 <?php // param tables disabled, removed the FALSE AND below to activate ?>
 <?php if ( FALSE AND $doc->params): ?>
-<?php //if (false): ?>
 <h5>Parameters</h5>
 <table>
 <tr>
@@ -36,7 +34,7 @@
 <?php endif ?>
 
 <?php if ($doc->return): ?>
-<h5>Returns:</h5>
+<h5>Return Values</h5>
 <ul class="return">
 <?php foreach ($doc->return as $set): list($type, $text) = $set; ?>
 <li><code><?php echo HTML::chars($type) ?></code> <?php echo HTML::chars($text) ?></li>
@@ -46,10 +44,9 @@
 
 <?php if ($doc->source): ?>
 <div class="method-source">
-<h6>Source Code:</h6>
+<h5>Source Code</h5>
 <pre><code><?php echo HTML::chars($doc->source) ?></code></pre>
 </div>
 <?php endif ?>
 
-<?php if ($doc->tags) echo View::factory('userguide/api/tags')->set('tags', $doc->tags) ?>
 </div>
