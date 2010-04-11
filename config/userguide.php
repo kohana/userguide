2fee7ea2fc829222f4eb8b70fa7126ae4397369e
Woody Gilk <woody.gilk@kohanaphp.com>
2010-04-11 06:30:58 +0200

    Normalized all "OR die" to use lowercase "or"

--- config/userguide.php
+++ config/userguide.php
@@ -1,15 +1,8 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
+<?php defined('SYSPATH') or die('No direct access allowed.');
 
 return array
 (
 	// Default the userguide language.
-	'lang'        => 'en-us',
-	
-	// Enable the API browser.  TRUE or FALSE
-	'api_browser' => TRUE,
-	
-	// Enable these packages in the API browser.  TRUE for all packages, or a string of comma seperated packages, using 'None' for a class with no @package
-	// Example: 'api_packages' => 'Kohana,Kohana/Database,Kohana/ORM,None',
-	'api_packages' => TRUE,
+	'lang'		=> 'en-us',
 );
 
