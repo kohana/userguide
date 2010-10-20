bec284b8262a54807d82625978d715b4d57892e6
bluehawk <phroughy@gmail.com>
2010-10-19 22:31:34 -0600

    Made h2 and h3 bigger and added underlines.   Added more line-height to pre code blocks

--- media/guide/js/kodoc.js
+++ media/guide/js/kodoc.js
@@ -4,12 +4,11 @@ $(document).ready(function()
 	// Syntax highlighter
 	$('pre:not(.debug) code').each(function()
 	{
-		$(this).addClass('brush: php');
+		$(this).addClass('brush: php, class-name: highlighted');
 	});
 
 	SyntaxHighlighter.config.tagName = 'code';
 	// Don't show the toolbar or line-numbers.
-	SyntaxHighlighter.defaults.toolbar = false;
 	SyntaxHighlighter.defaults.gutter = false;
 	SyntaxHighlighter.all();
 	
