247129b78cf07c45f355f4c8c232ae3d91a262a3
Woody Gilk <woody.gilk@kohanaframework.org>
2010-12-22 01:54:51 -0600

    Cleaned up page table of contents style

--- views/userguide/page-toc.php
+++ views/userguide/page-toc.php
@@ -1,16 +1,15 @@
-<div class="page-toc open">
-	<p class="header"><strong>Page Contents</strong></p>
+<div class="page-toc">
+	<p class="bottom"><strong>Page Contents</strong></p>
 	<div class="content">
-	<?php if (is_array($array)) : ?>
+	<?php if (is_array($array)): ?>
 		<?php foreach ($array as $item): ?>
-		
-			<?php for ($i = 1; $i < $item['level']; $i++) echo "&nbsp;&nbsp;&nbsp;" ?>
-		
+			<?php if ($item['level'] > 1): ?>
+			<?php echo str_repeat('&nbsp;', ($item['level'] - 1) * 4) ?>
+			<?php endif ?>
 			<?php echo HTML::anchor('#'.$item['id'],$item['name']); ?><br />
-		
 		<?php endforeach; ?>
 	<?php else: ?>
 		<span>nothing</span>
-	<?php endif; ?>
+	<?php endif ?>
 	</div>
 </div>
