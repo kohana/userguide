ed29b70e6b13bb2414951a1c0629b205135ec409
Woody Gilk <woody.gilk@kohanaframework.org>
2010-12-29 23:45:30 -0600

    Remove toggling the page TOC, it really doesn't add anything benefical to the interface

--- views/userguide/page-toc.php
+++ views/userguide/page-toc.php
@@ -1,13 +1,10 @@
 <?php if (is_array($array)): ?>
 <div class="page-toc">
-	<p class="top bottom"><strong>Page Contents</strong></p>
-	<div class="content">
-		<?php foreach ($array as $item): ?>
-			<?php if ($item['level'] > 1): ?>
-			<?php echo str_repeat('&nbsp;', ($item['level'] - 1) * 4) ?>
-			<?php endif ?>
-			<?php echo HTML::anchor('#'.$item['id'],$item['name']); ?><br />
-		<?php endforeach; ?>
-	</div>
+	<?php foreach ($array as $item): ?>
+		<?php if ($item['level'] > 1): ?>
+		<?php echo str_repeat('&nbsp;', ($item['level'] - 1) * 4) ?>
+		<?php endif ?>
+		<?php echo HTML::anchor('#'.$item['id'],$item['name']); ?><br />
+	<?php endforeach; ?>
 </div>
 <?php endif ?>
