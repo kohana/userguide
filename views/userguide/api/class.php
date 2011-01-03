2acbfc8b470ebff43925385d2b5db6385528e8af
Andrew Coulton <andrewnfcoulton@gmail.com>
2011-01-03 03:17:05 +0000

    Implement @3486 show class interfaces in API browser

--- views/userguide/api/class.php
+++ views/userguide/api/class.php
@@ -5,6 +5,18 @@
 	<?php endforeach; ?>
 </h1>
 
+<?php if ($interfaces = $doc->class->getInterfaceNames()):?>
+<p class="interfaces"><small>
+Implements:
+<?php
+for ($i = 0, $split = false, $count = count($interfaces); $i < $count; $i++, $split=" | ")
+{
+    echo $split . HTML::anchor($route->uri(array('class' => $interfaces[$i])),$interfaces[$i]);
+}
+?></small>
+</p>
+<?php endif;?>
+
 <?php echo $doc->description() ?>
 
 <?php if ($doc->tags): ?>
