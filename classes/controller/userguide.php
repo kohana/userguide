e0b0d20e947bf7095a882a515fac3e0e7460d2bd
Kiall Mac Innes <kiall@managedit.ie>
2011-09-23 17:20:54 +0100

    Fixing CS Issues. Uppercase Constants and Logical Operators

--- classes/controller/userguide.php
+++ classes/controller/userguide.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 /**
  * Kohana user guide and api browser.
  *
--- init.php
+++ init.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php defined('SYSPATH') OR die('No direct script access.');
 
 // Static file serving (CSS, JS, images)
 Route::set('docs/media', 'guide/media(/<file>)', array('file' => '.+'))
