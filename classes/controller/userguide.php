a185e310a88308699b8c121cb0ae427a0246e23b
Kiall Mac Innes <kiall@managedit.ie>
2012-01-10 18:24:18 +0000

    Update userguide controller for $response->check_cache() changes. Refs #4373

--- classes/controller/userguide.php
+++ classes/controller/userguide.php
@@ -258,7 +258,7 @@ class Controller_Userguide extends Controller_Template {
 		if ($file = Kohana::find_file('media/guide', $file, $ext))
 		{
 			// Check if the browser sent an "if-none-match: <etag>" header, and tell if the file hasn't changed
-			$this->response->check_cache(sha1($this->request->uri()).filemtime($file), $this->request);
+			$this->check_cache(sha1($this->request->uri()).filemtime($file));
 			
 			// Send the file content as the response
 			$this->response->body(file_get_contents($file));
